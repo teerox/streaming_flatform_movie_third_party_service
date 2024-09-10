7b7cd19c1e45d44976faf75f3a78d933cf184a83
teerox44 <anthony@publiclyme.com>
2024-09-09 21:33:25 -0300

    setup movie third party api

---
+++ src/main/java/com/example/moviethirdpartyservice/MovieThirdPartyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviethirdpartyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieThirdPartyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieThirdPartyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moviethirdpartyservice/MovieThirdPartyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviethirdpartyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieThirdPartyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
